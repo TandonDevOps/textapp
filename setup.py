19a3ef7b81c226c8ce3baef2f5c3d933adb54e23
gcallah <gcallah@mac.com>
2021-03-14 04:12:10 -0400

    Trying new package designation.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gcallah/text_menu",
-    packages=['text_menu'],
+    packages=['.'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
