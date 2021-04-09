f0b73421f7819be29b5fb0bda510c0ee0c616f48
HelalChow <hc2324@nyu.edu>
2021-04-09 15:52:48 -0400

    packaging to pypi

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setuptools.setup(
     description="Create simple but useful terminal menus.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gcallah/text_menu",
-    packages=['text_menu'],
+    url="https://github.com/TandonDevOps/textapp",
+    packages=['textapp'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
