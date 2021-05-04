8d4f737e6b7fde7d0ecd9854be7dc08e99cb151d
Gene Callahan <gcallah@mac.com>
2021-05-04 15:30:25 -0400

    bump_version scrip looks to be working.

--- bump_version.sh
+++ bump_version.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+export OUT_FILE=setup.test  # replace with setup.py when convinced
 
 echo "Enter the new version number:"
 read new_version
-cat setup.py | sed -e 's/version="[0-9.]*"/version="$new_version"/'
+cat setup.py | sed -e 's/version="[0-9.]*"/version="'$new_version'"/' > $OUT_FILE
